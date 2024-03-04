4cd3edb667568035bca062448584000115b83ab8
vardan <96.vardan.khachatryan@gmail.com>
2024-03-04 19:15:54 +0400

    Задача 2

---
+++ les_2_Простые Алгоритмы/N2/Program.cs
@@ -0,0 +1,18 @@
+﻿ // Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+ int x=0;
+ int y=0;
+ if (x > 0 && y > 0){
+Console.Write("1");
+ }
+else if (x < 0 && y > 0){
+Console.WriteLine("2");
+}
+else if (x < 0 && y < 0){
+Console.WriteLine("3");
+}
+else if (x > 0 && y < 0){
+Console.WriteLine("4");
+}
+else
+Console.WriteLine("Точка в центре");
