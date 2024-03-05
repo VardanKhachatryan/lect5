494f313daeec6fa72c96d00379c8d9515b21be3b
vardan <96.vardan.khachatryan@gmail.com>
2024-03-05 08:26:09 +0400

    задача3

---
+++ les_2_Простые Алгоритмы/N3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number=44;
+int firstNumber = number/10;
+int secondNumber = number % 10;
+if (firstNumber>secondNumber){
+    Console.Write(firstNumber);
+}
+else if(firstNumber<secondNumber){
+Console.Write(secondNumber);
+}
+else Console.Write(secondNumber);
