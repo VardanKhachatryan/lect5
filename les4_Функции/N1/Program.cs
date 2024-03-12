630c0dc70b6223bb997430f31411673928700b55
vardan <96.vardan.khachatryan@gmail.com>
2024-03-12 08:48:09 +0400

    add N1

---
+++ les4_Функции/N1/Program.cs
@@ -0,0 +1,28 @@
+﻿while (true)
+{
+Console.Write("Введите число или 'q' для выхода: ");
+string input = Console.ReadLine();
+if (input == "q")
+{
+break;
+}
+int number;
+if (int.TryParse(input, out number))
+{
+int sum = 0;
+while (number > 0)
+{
+sum += number % 10;
+number /= 10;
+}
+if (sum % 2 == 0)
+{
+Console.WriteLine("[THE AND]");
+break;
+}
+}
+else
+{
+Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+}
+}
